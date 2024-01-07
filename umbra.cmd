b164974fda4bc963f06f260a4d4120e7c7cf8394
D. Bohdan <dbohdan@dbohdan.com>
2024-01-07 11:02:34 +0000

    feat(umbra.cmd): add switch `/t` or `/text`

--- umbra.cmd
+++ umbra.cmd
@@ -1,1 +1,14 @@
-@python3 -O -m umbra.UmbraTk %*
+@echo off
+
+set module=UmbraTk
+
+if "%1"=="/t" (
+   set module=UmbraText
+   shift
+)
+if "%1"=="/text" (
+   set module=UmbraText
+   shift
+)
+
+python3 -O -m umbra.%module% %1 %2 %3 %4 %5 %6 %7 %8 %9
