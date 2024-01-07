07247b510f38d9b40bbe941794da20246e92faca
D. Bohdan <dbohdan@dbohdan.com>
2024-01-07 17:15:58 +0000

    feat(umbra.sh): improve arg parsing, log naming
    Do not rename old logs.
    Name logs by the date-time from the start;
    symlink `um.log` to the latest.
    
    Check that the Python command is available.
    Allow the user to choose the Python command
    through an environment variable.

--- umbra.sh
+++ umbra.sh
@@ -1,29 +1,37 @@
 #! /bin/sh
 set -eu
 
-module=UmbraTk
-if [ "${1:-}" = -t ] || [ "${1:-}" = --text ]; then
-    module=UmbraText
-    shift
+log_dir=logs
+python="${PYTHON:-python3}"
+
+if ! command -v "$python" > /dev/null; then
+    printf 'Python not found (tried "%s")\n' "$python" > /dev/stderr
+    exit 1
 fi
 
 log=0
-if [ "${1:-}" = -l ] || [ "${1:-}" = --log ]; then
-    log=1
-    shift
-fi
+module=UmbraTk
 
-if [ "$log" -eq 1 ]; then
-    log_dir=logs
-    log_path="$log_dir"/um.log
-
-    mkdir -p "$log_dir"
-
-    if [ -e "$log_path" ]; then
-        mv "$log_path" "$log_path"."$(stat -c %Y "$log_path")"
+while echo "${1:-}" | grep -E '^(-l|--log|-t|--text)$' > /dev/null; do
+    if [ "${1:-}" = -t ] || [ "${1:-}" = --text ]; then
+        module=UmbraText
+        shift
     fi
 
-    python3 -O -m umbra."$module" "$@" 2>&1 | tee "$log_path"
+    if [ "${1:-}" = -l ] || [ "${1:-}" = --log ]; then
+        log=1
+        shift
+    fi
+done
+
+if [ "$log" -eq 1 ]; then
+    log_filename=$(date '+%F-%T%z').log
+    log_path="$log_dir"/"$log_filename"
+
+    mkdir -p "$log_dir"
+    ln -f -s "$log_filename" "$log_dir"/um.log
+
+    "$python" -O -m umbra."$module" "$@" 2>&1 | tee "$log_path"
 else
-    python3 -O -m umbra."$module" "$@"
+    "$python" -O -m umbra."$module" "$@"
 fi
